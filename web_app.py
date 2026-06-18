b192fb97114cf8398255a672511e55029899dcd3
s-asuthker <siddharth.asuthker@icloud.com>
2026-06-18 10:55:15 -0500

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -3,7 +3,7 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 from flask import Flask, render_template, request, jsonify
-import io, base64, datetime, time
+import io, base64, datetime, time, os
 from datetime import date
 from forex_python.converter import CurrencyRates
 matplotlib.use('Agg')
